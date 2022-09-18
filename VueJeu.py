982c1366dbe94cd4fcc74144982130b1a777b5ac
plgdz <40022026+plgdz@users.noreply.github.com>
2022-09-18 10:52:24 -0400

    Update VueJeu.py

--- VueJeu.py
+++ VueJeu.py
@@ -12,28 +12,53 @@ class VueJeu :
     def printBottom() :
         bottom = "\t"
         for x in range(0, 33) :
-            bottom += "\u203e"
+            bottom += "\u203e"  # char upperscore
         print(bottom)
 
-    def printLigneDamier(lig, cellules) :
-        print("")
+    def printLigneDamier(ligne, cellules) :
+        print("\t| " + cellules[ligne][0] + " | " + cellules[ligne][1] + " | "  + cellules[ligne][2] + " | " + cellules[ligne][3] + " | " + cellules[ligne][4] + " | " + cellules[ligne][5] + " | " + cellules[ligne][6] + " | " + cellules[ligne][7] + " |")
 
     def printDamier(cellules) :
         VueJeu.printTop()
-        VueJeu.printLigneVide()
+        VueJeu.printLigneDamier(0, cellules)
         VueJeu.printLigneSeparation()
-        VueJeu.printLigneVide()
+        VueJeu.printLigneDamier(1, cellules)
         VueJeu.printLigneSeparation()
-        VueJeu.printLigneVide()
+        VueJeu.printLigneDamier(2, cellules)
         VueJeu.printLigneSeparation()
-        VueJeu.printLigneVide()
+        VueJeu.printLigneDamier(3, cellules)
         VueJeu.printLigneSeparation()
-        VueJeu.printLigneVide()
+        VueJeu.printLigneDamier(4, cellules)
         VueJeu.printLigneSeparation()
-        VueJeu.printLigneVide()
+        VueJeu.printLigneDamier(5, cellules)
         VueJeu.printBottom()
+
+    def showGameOver() :
+        topBoite = "|"
+        for x in range(0, 13) :
+            topBoite += "\u203e"  # char upperscore
+        topBoite += "|"
+        print(topBoite)
+        print("|  GAME OVER  |")
+        print("|_____________|")
+
+    def showNextLevel() :
+        topBoite = "|"
+        for x in range(0, 14) :
+            topBoite += "\u203e"  # char upperscore
+        topBoite += "|"
+        print(topBoite)
+        print("|  NEXT LEVEL  |")
+        print("|______________|")
+
 
 # POUR TEST
 if __name__ == "__main__" :
     grille = [] 
-    VueJeu.printDamier(grille)
+    for ligne in range(0, 6) :
+        cellule = []
+        grille.append(cellule)
+        for colone in range(0, 8) :
+            cellule.append(str(colone))
+    VueJeu.showGameOver()
+    VueJeu.showNextLevel()
