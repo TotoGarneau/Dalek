7e0f124962411eb4d7af9d47e670263aa52e6f6c
unknown <nancy.wlodarski@outlook.com>
2022-09-24 12:22:26 -0400

    Utilisez le main ici, il permet de naviguer dans les menus

--- daleks.py
+++ daleks.py
@@ -7,7 +7,7 @@ if __name__ == "__main__" :
         controller = ControllerMenu()
         choix = controller.choisirNiveau()
         if (choix == 1 or choix ==2 or choix == 3) :  # Si choisi un des 3 niveaux au menu
-            controller = JeuController()  #Appeler la classe JeuController 
+            controller = JeuController() 
             controller.start(choix)       #Appeler la fonction start de JeuController, passer le choix en paramètre
         if choix == 4 :
             choixQuitter = True
