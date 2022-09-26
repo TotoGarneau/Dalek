fdbe511fa43ce7f30c62a1d85968cbe00ff2b7bf
Veryba <maximegoaler@yahoo.com>
2022-09-26 19:03:08 -0400

    Test 2

--- Doc.py
+++ Doc.py
@@ -1,11 +1,34 @@
 import random
+from controlleursMenu import JeuController
+from Cellule import Grille
+
+
 class Doc:
 
-    def Docteur(self):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.life = True
+        self.Docteur = "W"
+
+    def move(self,x,y):
+        self.x = x
+        self.y = y
+
+    def death(self):
+        self.life = False
 
     def zappeur(self):
-        break
+        zap = []
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                zap.append([self.x + x, self.y + y])
+            if self.x == self.x+x and self.y == self.y+y:
+                zap.pop()
+
+        return zap
 
     def teleport(self):
-
-       if
+        y = random.randrange(0, 8)
+        x = random.randrange(0, 6)
+        return x, y
